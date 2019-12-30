e047a8b1f097f2a58da0329be1a1e59538d31bd5
x1tmwz <585702289+x1tmwz@users.noreply.github.com>
2019-12-30 15:34:15 +0200

    change j

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+let j =3;
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
