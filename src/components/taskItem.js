f17acbdaa04cf67fc3d2c47e0fdd355cd6640c0e
x1tmwz <585702289+x1tmwz@users.noreply.github.com>
2020-01-02 21:13:20 +0200

    Update taskItem.js

--- src/components/taskItem.js
+++ src/components/taskItem.js
@@ -9,12 +9,17 @@ class TaskItem extends React.Component {
         console.log(this.text);
         
         return <li>
-            <div>
-                {this.text}
+            <div onClick={this.dosomthing.bind(this.text)}> 
+               {this.text} 
             </div>
         </li>
     }
 
+}
+function dosomthing(text){
+    return text ="";
+ 
+    
 }
 
 export default TaskItem;
