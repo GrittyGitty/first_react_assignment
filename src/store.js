6f87737be0464ed21d57b944ad0c3b6ea420c316
GrittyGitty <58652339+GrittyGitty@users.noreply.github.com>
2020-01-08 19:29:59 +0200

    commit

--- src/store.js
+++ src/store.js
@@ -11,35 +11,36 @@ let initialState = {
     ]
 };
 
+
 const store = createStore((state = initialState, action) => {
     let { tasks } = state;
     let { payload, type } = action;
     console.log(tasks, payload, type);
     switch (type) {
         case "ADD_TASK":
-            return Object.assign({},state, {
+            return Object.assign({}, state, {
                 tasks: [...tasks, payload]
             });
         case "REMOVE_TASK":
-            return Object.assign({},state, {
+            return Object.assign({}, state, {
                 tasks: tasks.filter(task => task.text !== payload.text)
             });
         case "UPDATE_TASK":
-            return Object.assign({},state, {
+            return Object.assign({}, state, {
                 tasks: tasks.map(task =>
                     (task.text === payload.task.text) ?
                         new Task(payload.text) :
                         task)
             });
         case "TOGGLE_CROSS_TASK":
-            return Object.assign({},state, {
+            return Object.assign({}, state, {
                 tasks: tasks.map(task =>
                     (task.text === payload.text) ?
                         new Task(task.text, !task.isDone) :
                         task)
             });
         case "TOGGLE_EDIT_MODE":
-            return Object.assign({},state, {
+            return Object.assign({}, state, {
                 tasks: tasks.map(task =>
                     (task.text === payload.text) ?
                         task = new Task(task.text, false, !task.dblClicked) :
