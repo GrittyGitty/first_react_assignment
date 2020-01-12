b8812b99c9589df29fcbe3c5a78b2c374f50a91d
Diabl0269 <talefronny@gmail.com>
2020-01-12 16:50:26 +0200

    Add files via upload

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,67 +1,44 @@
 import React from 'react';
-import TaskItem from './taskItem';
-import Task from '../entities/task';
+import TaskItem from './TaskItem';
 
 class TaskList extends React.Component {
     constructor(props) {
         super(props);
-        this.tasks = props.tasksList;
-        this.toggleTaskByText = this.toggleTaskByText.bind(this);
-        this.tasks.push(new Task("buy pizza"));
+        this.state = { tasksList: props.tasksList };
+        this.deleteTask = this.deleteTask.bind(this);
     }
 
-    componentWillReceiveProps(props) {
-        this.tasks = props.tasksList;
-        this.forceUpdate();
+
+    static getDerivedStateFromProps(nextProps, prevState) {
+        if (prevState.tasksList !== nextProps.tasksList)
+            return { tasks: nextProps.tasksList }
+        else
+            return null;
     }
 
     render() {
         return <div>
             <ul>
-                {this.printTasks(this.tasks)}
+                {this.renderTasks(this.state.tasksList)}
             </ul>
         </div>
     }
 
-    printTasks(list) {
+    renderTasks(list) {
         let elements = [];
-        // console.log(list);
-        
-        for (let task of list) {
-            elements.push(<TaskItem text={task.text}></TaskItem>);
-            
-            // elements.push(<li key={task.text}>
-            //             <div onClick={this.toggleTaskByText}>
-            //                 {this.getTaskText(task)}
-            //             </div>
-            //         </li>)
+        for (let i = 0; i < list.length; i++) {
+            elements.push(<TaskItem key={"item" + i}
+                deleteTaskCallback={this.deleteTask} text={list[i].text} task={list[i]}></TaskItem>);
         }
-        // console.log(elements);
-        
         return elements;
     }
 
-
-
-    toggleTaskByText(event) {
-        console.log(event.target);
-        let text = "a";
-        for (let task of this.tasks) {
-            if (task.text === text) {
-                task.toggleTaskByText();
-            }
-        }
-        this.forceUpdate();
+    deleteTask(taskToDelete) {
+        let tasks = this.state.tasksList.filter(task => {
+            return task.key !== taskToDelete.key;
+        });
+        this.setState({tasksList: tasks});
     }
-
-    getTaskText(task) {
-        if (task.isDone) {
-            return <p><del>{task.text}</del></p>
-        }
-        else {
-            return <p>{task.text}</p>
-        }
-    }
-
 }
+
 export default TaskList;
